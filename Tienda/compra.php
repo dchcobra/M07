8baf55d82ac3aaec435e55f6ff5c6a3b0015631b
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-14 20:03:20 +0200

    CARRO

---
+++ Tienda/compra.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>COMPRA</title>
+</head>
+<body>
+    <center>
+
+
+    <?php 
+        echo '
+        <form action="afegeix_carro.php" method="POST">
+            <label> PRODUCTO: </label>
+            <select name="producto">
+                        $fichero = file("productes.txt");
+                        foreach ($fichero as $new) {
+                            $separados = explode(";", $new);
+                            <option>' . $separados[0] . '</option>';
+                        };     
+            </select>
+            <br>
+            <br>
+            <label>CANTIDAD:</label>
+            <input name="cantidad" type="number" min="1" />
+            <br>
+            <br>    
+            <input type="submit" value="ENVIAR" />
+
+        </form>';
+    ?>                    
+
+    </center>
+</body>
+</html>
