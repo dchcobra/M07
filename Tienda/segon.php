36d1c616c7a409e45a9fa537d90d5311d22ae7b7
David Castillla Henares <57146460+dchcobra@users.noreply.github.com>
2020-10-15 15:47:49 +0200

    PRUEBA CON SEGON.php

---
+++ Tienda/segon.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>SEGON</h3>
+    <?php 
+    session_start();
+
+        echo $_SESSION["producte"];
+    ?>
+</body>
+</html>
